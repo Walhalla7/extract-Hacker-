6bdb584671453e170ea471340d8ce17765fcfe5b
Walhalla7 <bartoszrdzanekwp@gmail.com>
2025-03-28 12:14:57 -0400

    Base Movement And test Arena

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 6f, gravityModifier = 2f, jumpPower = 8f, runSpeed = 12f;
+    public CharacterController charCon;
+
+    private Vector3 moveInput;
+    private float yVelocity;
+
+    public Transform camTrans;
+    public float mouseSensitivity = 2f;
+    public bool invertX, invertY;
+    public float maxLookAngle = 80f; // Prevents camera flipping
+
+    void Update()
+    {
+        // Horizontal movement
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horiMove = transform.right * Input.GetAxis("Horizontal");
+        moveInput = (horiMove + vertMove).normalized;
+
+        // Running
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : moveSpeed;
+        moveInput *= currentSpeed;
+
+        // Apply gravity
+        if (charCon.isGrounded)
+        {
+            yVelocity = -1f; // Small downward force to keep grounded check stable
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                yVelocity = jumpPower;
+            }
+        }
+        else
+        {
+            yVelocity += Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        moveInput.y = yVelocity;
+        charCon.Move(moveInput * Time.deltaTime);
+
+        // Mouse Look
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+        if (invertX) mouseInput.x = -mouseInput.x;
+        if (invertY) mouseInput.y = -mouseInput.y;
+
+        transform.Rotate(0f, mouseInput.x, 0f);
+        
+        float camRotationX = camTrans.localEulerAngles.x - mouseInput.y;
+        if (camRotationX > 180f) camRotationX -= 360f;
+        camRotationX = Mathf.Clamp(camRotationX, -maxLookAngle, maxLookAngle);
+
+        camTrans.localEulerAngles = new Vector3(camRotationX, 0f, 0f);
+    }
+}
