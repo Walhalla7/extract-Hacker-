d8059ed4d87cdc25623448fedb0aca712574edbf
CGAltman <ialtman@clemson.edu>
2025-04-17 16:36:20 -0400

    Total Layout Blocking

--- Assets/Scripts/KeyCardScript.cs
+++ Assets/Scripts/KeyCardScript.cs
@@ -7,12 +7,18 @@ public class KeyCardScript : MonoBehaviour
 
     // ============================================================== Variables =====================================================
     // Object References    
+    public int doorAmount;
     public DoorScript doorRef;
+    //public List<DoorScript> Doors;
 
     // ============================================================== Start =====================================================
     // Lock the door
     void Start()
     {
+        /*foreach (DoorScript d in Doors)
+        {
+            d.LockDoors();
+        }*/
         doorRef.LockDoors();
     }
 
